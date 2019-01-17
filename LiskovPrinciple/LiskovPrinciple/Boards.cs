59e9d92d1df93a284bbe603881f2ef9faf815891
Stanley Cohen <scohe001@ucr.edu>
2019-01-17 15:30:46 -0600

    Added the Board Liskov Programming example

---
+++ LiskovPrinciple/LiskovPrinciple/Boards.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LiskovPrinciple
+{
+    class Board
+    {
+        int Height { get; set; }
+        int Width { get; set; }
+        int[][] Tiles { get; set; }
+
+        public void AddUnit(int unit, int x, int y) { }
+        public int GetTile(int x, int y) { return -1; }
+        public int RemoveUnit(int x, int y) { return -1; }
+    }
+    
+    class ThreeDBoard : Board
+    {
+        int Depth { get; set; }
+        int[][][] ThreeDTiles { get; set; }
+
+        public void AddUnit(int unit, int x, int y, int z) { }
+        public int GetTile(int x, int y, int z) { return -1; }
+        public int RemoveUnit(int x, int y, int z) { return -1; }
+    }
+}
