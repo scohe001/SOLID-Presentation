43213b2bf867ee23da11938c0aee12d7e4ed5357
Stanley Cohen <scohe001@ucr.edu>
2019-01-17 13:54:11 -0600

    Updated Liskov with a square/rectangle example

---
+++ LiskovPrinciple/LiskovPrinciple/Shapes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LiskovPrinciple
+{
+    class Rectangle
+    {
+        public int Length { get; set; }
+        public int Width { get; set; }
+
+        public int getArea() { return Length * Width; }
+    }
+
+    class Square : Rectangle
+    {
+        //Special square things...
+    }
+}
